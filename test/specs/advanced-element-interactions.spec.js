56d57643245bbfb337132f7c037cf25f87a8cc08
hyper <tvrdisica@gmail.com>
2023-07-14 10:40:25 +0200

    Advanced elements interactions

--- test/specs/advanced-element-interactions.spec.js
+++ test/specs/advanced-element-interactions.spec.js
@@ -82,4 +82,78 @@ describe('advanced element interactions - examples', () => {
     await firstLink.click();
     // await browser.pause(3000);
   });
+
+  it('handling windows', async () => {
+    await browser.url('/');
+    await browser.newWindow('https://automationteststore.com/');
+
+    let currentWindow_Title = await browser.getTitle();
+    console.log(`>>Current window Title: ${currentWindow_Title}`);
+    await expect(browser).toHaveUrlContaining('automationteststore');
+
+    await browser.switchWindow('webdriveruniversity.com');
+    let parentWindow_Title = await browser.getTitle();
+    console.log(`>>Parent window Title: ${parentWindow_Title}`);
+    await expect(browser).toHaveUrlContaining('webdriveruniversity');
+
+    await $('#contact-us').click();
+    await browser.switchWindow('automationteststore');
+    await browser.closeWindow();
+    await browser.switchWindow('contactus');
+    await browser.closeWindow();
+    await browser.switchWindow('webdriveruni');
+    console.log(await browser.getTitle());
+    // await browser.pause(3000);
+  });
+
+  it('IFrames handling', async () => {
+    await browser.url('/IFrame/index.html');
+    const iframe = await $('#frame');
+    await browser.switchToFrame(iframe);
+    await $("//a[text()='Our Products']").click();
+    // await browser.pause(3000);
+    await browser.switchToParentFrame();
+    // await browser.pause(3000);
+  });
+
+  it('Alerts handling', async () => {
+    await browser.url('/Popup-Alerts/index.html');
+    await $('#button1').click();
+    await browser.acceptAlert();
+
+    await $('#button4').click();
+    const alertText = await browser.getAlertText();
+    await expect(alertText).toEqual('Press a button!');
+
+    await browser.acceptAlert();
+    await expect($('#confirm-alert-text')).toHaveText('You pressed OK!');
+    // await browser.pause(3000);
+
+    await $('#button4').click();
+    await browser.dismissAlert();
+    await expect($('#confirm-alert-text')).toHaveText('You pressed Cancel!');
+    // await browser.pause(3000);
+  });
+
+  it('FILE UPLOADS handling', async () => {
+    await browser.url('/File-Upload/index.html');
+    await $('#myFile').addValue(`${process.cwd()}//data//dummy_file.txt`);
+    // await browser.pause(2000);
+    await $('#submit-button').click();
+    const alertText = await browser.getAlertText();
+    await expect(alertText).toEqual('Your file has now been uploaded!');
+    // await browser.pause(2000);
+  });
+
+  it('Injecting JS code in a website', async () => {
+    await browser.url('/Hidden-Elements/index.html');
+    await browser.execute(() => {
+      return document.getElementById('not-displayed').setAttribute('id', '');
+    });
+
+    await browser.execute(() => {
+      return (document.body.style.backgroundColor = 'tomato');
+    });
+    await browser.pause(3000);
+  });
 });
