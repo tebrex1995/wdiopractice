b81c2074858735f7034377ae3004127340749c19
hyper <tvrdisica@gmail.com>
2023-07-27 12:45:45 +0200

    fixed

--- test/pageObjects/automation-test-store/cart.page.js
+++ test/pageObjects/automation-test-store/cart.page.js
@@ -2,11 +2,11 @@ import BasePage from './base.page';
 
 class CartPage extends BasePage {
   get shippingRate() {
-    $("//span[text()='Flat Shipping Rate:']/../following-sibling::td");
+    return $("//span[text()='Flat Shipping Rate:']/../following-sibling::td");
   }
 
   get cartTotal() {
-    $("//span[text()='Total:']/../following-sibling::td");
+    return $("//span[text()='Total:']/../following-sibling::td");
   }
 }
 
